51fb276bb005f8ca77f1e0ca49afbce6f553ff5a
Sebastian Vesterlund <aletheos@outlook.com>
2020-02-19 20:11:00 +0100

    Begin work on basic character controller
    Began work on player controller script, and made some
    changes to my sandbox scene.
    
    Added some files:
     a script for player movement(currently does nothing)
     a nice looking, but sadly not free, font
     Textmesh Pro files(should be used instead of built-in text)
    
    Removed the dummy script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+public class PlayerController : MonoBehaviour {
+
+	[System.Serializable]
+	public class PlayerProperties {
+		[Range(0f, 100f)]
+		public float walkingSpeed;
+		[Range(0f, 100f)]
+		public float runningSpeed;
+		[Range(0f, 100f)]
+		public float acceleration;
+		[Range(0f, 100f)]
+		public float jumpForce;
+	}
+
+	[System.Flags]
+	private enum PlayerInputs {
+		MOVE_LEFT,
+		MOVE_RIGHT,
+		JUMP
+	}
+
+	private Rigidbody2D body;
+	private new CapsuleCollider2D collider;
+	public PlayerProperties properties;
+	private int inputs;
+
+	void Start() {
+		body = GetComponent<Rigidbody2D>();
+		collider = GetComponent<CapsuleCollider2D>();
+	}
+
+	private void OnCollisionStay2D(Collision2D collision) {
+		
+	}
+	
+	void Update() {
+	
+	}
+
+	private void FixedUpdate() {
+		
+	}
+}
